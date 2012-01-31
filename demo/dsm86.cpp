ed8c3c14ac5861c7858bdb460b5506e51ee3f114
Makoto Nishiura <nishiuramakoto@gmail.com>
2012-02-01 00:56:34 +0900

    demo programs

---
+++ demo/dsm86.cpp
@@ -0,0 +1,57 @@
+#include <erasm/dsm_x86_util.hpp>
+#include <erasm/dsm_x86.hpp>
+#include <erasm/faststream.hpp>
+
+using namespace std;
+using namespace erasm::x86;
+using namespace erasm::x86::addr32::data32;
+
+struct MyDsm : public SimpleDsm<>
+{
+   MyDsm(const_code_ptr start,ostream& os = erasm::cout)
+      : SimpleDsm(start,os)
+      {}
+
+   using SimpleDsm::action;
+
+   action_result_type
+   action(const Ret& insn)
+      {
+	 os() << "Matched:" << Ret::mnemonic << endl;
+	 return finish(insn);
+      }
+
+   template<class Op>
+   action_result_type
+   action(const Push& insn,const Op& op)
+      {
+	 os() << "Matched:" << Push::mnemonic
+	      << " " << op << endl;
+	 return next(insn);
+      }
+
+};
+
+int f(int x)
+{
+   return x+1;
+}
+
+int main(int argc,char**argv)
+{
+   const_code_ptr p = (const_code_ptr) f;
+   MyDsm   mydsm(p);
+
+   const_code_ptr end = decode(p,mydsm);
+
+   if (mydsm.failed()) {
+      erasm::cout << "Disassembly failed at:" 
+		  << hex << (void*)end << endl;
+   } else {
+      erasm::cout << "Disassembly ended at:" 
+		  << hex << (void*)end << endl;
+   }
+   return 0;
+}
+
+
